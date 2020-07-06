68edea1b00f43f0d5055ea49cdd198d2c316a485
gamedevleo@gmail.com <gamedevleo@gmail.com>
2020-07-06 13:19:12 +1000

    first commit

---
+++ Assets/Scripts/InstantiateLeft.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateLeft : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject spawnPointLeft;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(prefab, new Vector3(spawnPointLeft.transform.position.x,spawnPointLeft.transform.position.y,0), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/InstantiateRight.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateRight : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject spawnPointRight;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Instantiate(prefab, new Vector3(spawnPointRight.transform.position.x,spawnPointRight.transform.position.y,0), Quaternion.identity);
+    }
+}
